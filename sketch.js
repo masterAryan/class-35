0f67fad7b1ad65fbfe9da172862c32a63af62f91
masterAryan <66153461+masterAryan@users.noreply.github.com>
2020-08-21 08:40:36 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canvas,bgimg;
+var gameState = 0;
+var playerCount,database;
+var form,player,game;
+
+function setup(){
+  createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+
+}
+
+function draw(){
+ 
+  
+}
+
